97e7df0fe7ddcecd0162d7cffba86b31b2b68dc4
Jacob Doren <jakedoren@gmail.com>
2021-07-08 20:59:25 -0400

    added custom contact form service functionality

--- src/components/contactform/Contactform.js
+++ src/components/contactform/Contactform.js
@@ -1,33 +1,66 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Contactform.css'
 import ThemeContext from '../../ThemeContext'
+import axios from 'axios'
 
 
 const Contactform = () => {
     const {dark} = React.useContext(ThemeContext);
 
-    const handleSubmit = () => {
-        alert('Message recieved, I will get back to you shortly!')
+    const [ formInfo, setFormInfo ] = useState({
+        name: '',
+        email: '',
+        message: ''
+    })
+
+    const [ formResponse, setFormResponse ] = useState('')
+    const [ error, setError ] = useState(false)
+
+    const handleChange = (e) => {
+        setFormInfo({
+            ...formInfo, [e.target.name] : e.target.value
+        })
+    }
+
+    const  handleSubmit = async (e) => {
+       e.preventDefault()
+
+       axios.post('http://localhost:8080/', formInfo)
+            .then((result) => {
+                setFormResponse(result.data.success)
+                setFormInfo({
+                    name: '',
+                    email: '',
+                    message: ''
+                })
+            })
+            .catch((err) => {
+                setFormResponse(err.response.data.errorMessage)
+                
+            })
+         
+        
+    
     }
 
     return (
         <div className={dark ? "contactform contactform-dark" : "contactform"}>
             <div className="contactform-wrapper">
-                <form method="POST" data-netlify="true" className={dark ? "form-dark form" : "form"} name="contact" onSubmit={handleSubmit}>
+                <form className={dark ? "form-dark form" : "form"} >
                     <div>
                         <h1 style={{color: dark ? "white" : null}}>Get In Touch</h1>
+                        {formResponse && <p>{formResponse}</p>}
                     </div>
-                    <input type="hidden" name="form-name" value="contact" />
                     <div className="name">
-                       <input type="text" name="name" id="name" placeholder="name" className={dark ? "input-dark" : "input-light"} /> 
+                       <input type="text" name="name" id="name" placeholder="name" className={dark ? "input-dark" : "input-light"} onChange={ handleChange} value={formInfo.name} /> 
                     </div>
                     <div className="email">
-                        <input type="email" name="email" id="email" placeholder="email" className={dark ? "input-dark" : "input-light"} />
+                        <input type="email" name="email" id="email" placeholder="email" className={dark ? "input-dark" : "input-light"} onChange={ handleChange} value={formInfo.email} />
                     </div>
                     <div className="message">
-                        <textarea name="message" id="message" placeholder="message" rows="7" className={dark ? "input-dark" : "input-light"}/>
+                        <textarea name="message" id="message" placeholder="message" rows="7" className={dark ? "input-dark" : "input-light"} onChange={ handleChange} value={formInfo.message} />
                     </div>
-                    <button className={dark ? "submit submit-dark" : "submit"} type="submit">
+                    <button className={dark ? "submit submit-dark" : "submit"} onClick={handleSubmit} >
                         <h1 style={{color: dark ? "white" : null}}>Submit</h1>
                     </button>
                 </form>
