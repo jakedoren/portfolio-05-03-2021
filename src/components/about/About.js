644560bd32660815813471a65216af76b9435b9d
Jacob Doren <jakedoren@gmail.com>
2021-10-03 21:31:45 -0400

    tech stuff and stack section complete

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import './About.scss'
 import fluffyPal from '../../assets/fluffyfriend.JPG'
+import MongoDbImage from '../../assets/tech/mongo.png'
+import ExpressImage from '../../assets/tech/express.png'
+import ReactImage from '../../assets/tech/react.png'
+import NodeImage from '../../assets/tech/node.png'
 
 const About = () => {
     return (
@@ -13,7 +17,22 @@ const About = () => {
                         <h1>I'm Jake.</h1>
                     </div>
                 </div>
+                <div className="me-paragraph">
+                    <h2><strong>The Tech Stuff</strong></h2>
+                    <p>
+                        I am a full stack developer from sunny Orlando, Florida with a passion for writing elegant code and building 
+                        real-world software. Development is more than just a job for me, it's also one of my hobbies. I find learning new technologies, 
+                        building, creating and solving problems to be very gratifying. I recognize these skills to be of up-most importance for businesses 
+                        large and small alike and plan to leverage these skills from within businesses to provide value.
+                    </p>
+                    <h2><strong>My Strongest Stack</strong></h2>
+                    <img src={MongoDbImage} alt="mongodb" />
+                    <img src={ExpressImage} alt="mongodb" />
+                    <img src={ReactImage} alt="mongodb" />
+                    <img src={NodeImage} alt="mongodb" />
+                </div>
             </div>
+            
         </div>
     )
 }
