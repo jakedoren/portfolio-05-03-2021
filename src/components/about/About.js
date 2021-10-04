0548a894e6826b05e3071553464d85d5dd4923b2
Jacob Doren <jakedoren@gmail.com>
2021-10-03 22:29:26 -0400

    finished

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -5,31 +5,36 @@ import MongoDbImage from '../../assets/tech/mongo.png'
 import ExpressImage from '../../assets/tech/express.png'
 import ReactImage from '../../assets/tech/react.png'
 import NodeImage from '../../assets/tech/node.png'
+import ThemeContext from '../../ThemeContext'
 
 const About = () => {
+    const {dark} = React.useContext(ThemeContext);
+
     return (
-        <div className="about">
+        <div className={dark ? "about-dark" : "about"}>
             <div className="about-wrap">
                 <div className="message-container">
                     <div className="left-text"><span><img src={fluffyPal} alt="my furry friend" /></span></div>
                     <div className="right-text">
                         <h2>Hey there!</h2>
-                        <h1>I'm Jake.</h1>
+                        <h1 style={dark ? null : {color: "#2b2b2b"}}>I'm Jake.</h1>
                     </div>
                 </div>
                 <div className="me-paragraph">
                     <h2><strong>The Tech Stuff</strong></h2>
-                    <p>
+                    <p style={dark ? null : {color: "#1f1f1f"}}>
                         I am a full stack developer from sunny Orlando, Florida with a passion for writing elegant code and building 
                         real-world software. Development is more than just a job for me, it's also one of my hobbies. I find learning new technologies, 
                         building, creating and solving problems to be very gratifying. I recognize these skills to be of up-most importance for businesses 
                         large and small alike and plan to leverage these skills from within businesses to provide value.
                     </p>
                     <h2><strong>My Strongest Stack</strong></h2>
-                    <img src={MongoDbImage} alt="mongodb" />
-                    <img src={ExpressImage} alt="express" />
-                    <img src={ReactImage} alt="react" />
-                    <img src={NodeImage} alt="node" />
+                    <div className="tech-images">
+                        <img src={MongoDbImage} alt="mongodb" />
+                        <img src={ExpressImage} alt="express" />
+                        <img src={ReactImage} alt="react" />
+                        <img src={NodeImage} alt="node" />
+                    </div>
                 </div>
             </div>
             
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -29,7 +29,7 @@ const Navbar = () => {
                             <Link to="/" className={dark ? "icons-dark" : null}><li className={dark ? "icons-dark" : null}>Home</li></Link>
                             <Link to="/work" ><li className={dark ? "icons-dark" : null}>Work</li></Link>
                             <Link to="/contact" ><li className={dark ? "icons-dark" : null}>Contact</li></Link>
-                            <Link to="/contact" ><li className={dark ? "icons-dark" : null}>About</li></Link>
+                            <Link to="/about" ><li className={dark ? "icons-dark" : null}>About</li></Link>
                         </div> :
                         null
                         }
